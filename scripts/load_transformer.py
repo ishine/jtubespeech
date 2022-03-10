8db6838e0f02c2b663ce704e5aff783bf6c21ad9
Daniel <nicholasneo78@gmail.com>
2022-03-10 10:31:39 +0800

    added test code for the voxlingua107

--- scripts/load_transformer.py
+++ scripts/load_transformer.py
@@ -1,12 +1,29 @@
 import torchaudio
 from speechbrain.pretrained import EncoderClassifier
 
-# load the voxlingua107 pretrained model from speechbrain
-language_id = EncoderClassifier.from_hparams(source="TalTechNLP/voxlingua107-epaca-tdnn", savedir="tmp")
-# Download Thai language sample from Omniglot and cvert to suitable form
-# signal = language_id.load_audio("https://omniglot.com/soundfiles/udhr/udhr_th.mp3")
-signal = language_id.load_audio("../barca_spanish.opus")
-prediction =  language_id.classify_batch(signal)
-print(prediction)
-print()
-print(prediction[3])
+class AudioLanguagePrediction():
+    
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def get_prediction(self):
+        # load the voxlingua107 pretrained model from speechbrain
+        language_id = EncoderClassifier.from_hparams(source="TalTechNLP/voxlingua107-epaca-tdnn", savedir="tmp")
+
+        # signal = language_id.load_audio("https://omniglot.com/soundfiles/udhr/udhr_th.mp3")
+        signal = language_id.load_audio(self.filepath)
+        prediction =  language_id.classify_batch(signal)
+
+        print(prediction)
+        print()
+        print(prediction[3])
+
+        #return prediction
+    
+    def __call__(self):
+        return self.get_prediction()
+
+if __name__ == '__main__':
+    FILEPATH = "../indo.opus"
+    lang_prediction = AudioLanguagePrediction(FILEPATH)
+    lang_prediction()
