48532288ff72c5b267359aef31a59100951da177
nicholasneo78 <nicholasneo78@gmail.com>
2022-05-17 11:10:13 +0800

    config update

--- get_video_id.py
+++ get_video_id.py
@@ -30,6 +30,17 @@ class GetVideoId:
         self.sleep = sleep
         self.is_limit = is_limit
 
+    def create_new_dir(self, directory: str) -> None:
+        '''
+            creates new directory and ignore already created ones
+
+            directory: the directory path that is being created
+        '''
+        try:
+            os.mkdir(directory)
+        except OSError as error:
+            pass # directory already exists!
+
     def get_video_id(self) -> None:
         '''
             gets the channel id text file and outputs the video id text file
@@ -37,6 +48,12 @@ class GetVideoId:
         # # check if the video id filename exists, if exists, delete it to prevent corruption in appending to the text file
         # if os.path.isfile(self.video_id):
         #     os.remove(self.video_id)
+
+        # make new directory to store the video_id
+        video_id_path_split_list = self.video_id.split("/")
+        self.create_new_dir(f"{video_id_path_split_list[0]}/") # create the main folder
+        self.create_new_dir(f"{video_id_path_split_list[0]}/{video_id_path_split_list[1]}") # create the sub folder
+
 
         # check if the video id filename exists, if it exists, throw an error to prevent corruption in appending to the text file
         assert not os.path.isfile(self.video_id), "video id filename already exists, please insert an alternative filename to avoid confusion!"
