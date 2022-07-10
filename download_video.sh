c003b54794fd447fd400c4d2677766c0387c212e
nicholasneo78 <nicholasneo78@gmail.com>
2022-07-10 20:33:11 +0800

    config edit

--- download_video.sh
+++ download_video.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LANGUAGE="id"
-BATCH_CSV_FILE="video_id_with_sub/id/id_small_demo/id_small_demo_batch_1.csv"
+BATCH_CSV_FILE="video_id_with_sub/id/id_small_demo/csv_batch/id_small_demo_batch_4.csv"
 OUTPUT_DIR="video"
 
 python3 download_video.py \
